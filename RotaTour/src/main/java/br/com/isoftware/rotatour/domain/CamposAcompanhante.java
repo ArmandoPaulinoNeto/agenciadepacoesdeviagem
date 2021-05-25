e771710ec5067859a471bf285cb07f0718969b31
Armando <armandopaulinosilvaneto@gmail.com>
2021-05-25 17:48:04 -0300

    Criação da class CamposAcompanhante

---
+++ RotaTour/src/main/java/br/com/isoftware/rotatour/domain/CamposAcompanhante.java
@@ -0,0 +1,24 @@
+package br.com.isoftware.rotatour.domain;
+
+public class CamposAcompanhante {
+		
+	private String nome;
+		
+	private String dataNacimento;
+		
+		public String getCampoNome() {
+		return nome;
+	}
+
+	public void setCampoNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCampoData() {
+		return dataNacimento;
+	}
+
+	public void setCampoData(String dataNacimento) {
+		this.dataNacimento = dataNacimento;
+	}
+}
